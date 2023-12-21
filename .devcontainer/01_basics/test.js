b1c4034792812620734891d79a766fc5c839e0cf
Anamikaguptaa <154416319+Anamikaguptaa@users.noreply.github.com>
2023-12-21 08:32:27 +0000

    hhhh

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("anamika")
+
+const accountId=12345
+var accountPasword="999"
+
+let accountEmail="anamika10@gmail.com"
+ accountEmail="raja@gmail.com"
+ accountcity="patna"
+
+
+console.log(accountEmail);
+
+accountEmail="raj@gmail"
+accountPassword="10"
+
+accountId="999999"
+accountcity="kolkata"
+console.table([accountEmail,accountcity,accountId,accountPassword])
+        
